48f7a4ca8e1b1d7b03b71f9a373797ee29972699
Juan Nunez <jemigdion@gmail.com>
2020-11-23 02:14:16 -0600

    Fixing lint errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is succesful"""
         email = 'test@london.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
+            email=email,
             password=password
         )
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -19,7 +20,7 @@ class ModelTests(TestCase):
         email = 'test@LONDON.com'
         password = 'Testpass1234'
         user = get_user_model().objects.create_user(
-            email = email,
+            email=email,
             password=password
         )
 
